2fbec0fcddc676942690a5df0c3bd17dd7375842
Qamza25 <unathyqamza@gmail.com>
2026-07-03 15:42:19 +0200

    Remove unused files

--- js/utils.js
+++
@@ -1,4 +0,0 @@
-const Utils = {
-  debounce(fn, delay = 200) { let t; return (...args) => { clearTimeout(t); t = setTimeout(() => fn(...args), delay); }; },
-  isValidEmail: (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-};
